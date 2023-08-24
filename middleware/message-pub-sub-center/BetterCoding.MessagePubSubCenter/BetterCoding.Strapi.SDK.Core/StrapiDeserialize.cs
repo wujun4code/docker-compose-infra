10b49594c4187569bc856cd941a01b2aab48c6a1
Wu Jun <wujun4code@gmail.com>
2023-08-25 06:35:58 +0800

    feat: create strapi sdk.

---
+++ middleware/message-pub-sub-center/BetterCoding.MessagePubSubCenter/BetterCoding.Strapi.SDK.Core/StrapiDeserialize.cs
@@ -0,0 +1,19 @@
+﻿using BetterCoding.Patterns.FactoryMethod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterCoding.Strapi.SDK.Core
+{
+    public interface IStrapiDeserialize : IProcessor<string, StrapiObject>
+    {
+
+    }
+
+    //public class StrapiStringDeserialize : IStrapiDeserialize
+    //{
+
+    //}
+}
---
+++ middleware/message-pub-sub-center/BetterCoding.MessagePubSubCenter/BetterCoding.Strapi.SDK.Core/StrapiObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterCoding.Strapi.SDK.Core
+{
+    public class StrapiObject
+    {
+        IDictionary<string, object> serverData = new Dictionary<string, object> { };
+    }
+}
